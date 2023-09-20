b461e7318444d509c328fee47c452cac03924399
Amirhossein <amirmsh@student.ubc.ca>
2023-09-19 21:31:18 -0700

    dynamic component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 
-import './App.css';
-import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=696ed5be";
 
+const movie1 = {
+  Title: "Superman, Spiderman or Batman",
+  Year: "2011",
+  imdbID: "tt2084949",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg",
+};
+
 const App = () => {
-    // Async because it takes some time to fetch these movies
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
+    const [movies, setMovies] = useState([]);
+  // Async because it takes some time to fetch these movies
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-        console.log(data.Search);
-    };
+    setMovies(data.Search);
+  };
   useEffect(() => {
-    searchMovies('Spiderman')
-
+    searchMovies("Spiderman");
   }, []);
 
-  return <h1>App</h1>;
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input placeholder="Search for movies" value="" onChange={() => {}} />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found!</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
