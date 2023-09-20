d5f7b3a19861e2424d5bcf1741588f8e2169ff68
Amirhossein <amirmsh@student.ubc.ca>
2023-09-19 21:39:08 -0700

    added the search component

--- src/App.js
+++ src/App.js
@@ -7,17 +7,10 @@ import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=696ed5be";
 
-const movie1 = {
-  Title: "Superman, Spiderman or Batman",
-  Year: "2011",
-  imdbID: "tt2084949",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg",
-};
-
 const App = () => {
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
+
   // Async because it takes some time to fetch these movies
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -34,8 +27,12 @@ const App = () => {
       <h1>MovieLand</h1>
 
       <div className="search">
-        <input placeholder="Search for movies" value="" onChange={() => {}} />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
       {movies?.length > 0 ? (
         <div className="container">
