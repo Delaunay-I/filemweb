f3595533c7a6defc33249b8a1be29a029ed0e583
Amirhossein <amirmsh@student.ubc.ca>
2023-09-19 20:36:20 -0700

    added style and search svg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import { React, useEffect } from "react";
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = "http://www.omdbapi.com?apikey=696ed5be";
 
 const App = () => {
-    return (
-        <h1>App</h1>
-    )
+    // Async because it takes some time to fetch these movies
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    };
+  useEffect(() => {
+    searchMovies('Spiderman')
+
+  }, []);
+
+  return <h1>App</h1>;
 };
 
 export default App;
